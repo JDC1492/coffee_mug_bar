1d50474dd8ad730bab283fd9635fecbec56c83eb
JDC1492 <Jonathan_Cohen10@yahoo.com>
2022-06-06 17:10:33 -0400

    setting up a schedule using arrays

--- practice.java
+++ practice.java
@@ -1,15 +1,21 @@
 import java.util.Arrays; 
 
 public class Practice{
-    int time;
+    double time;
+    double[] planner = new Array[7];
 
     public Practice(int time){
         this.time = time;
     }
 
     public static void main(String[] args){
-        Practice first = new Practice(6);
-        System.out.println(first.time + " PM is when practice will be held.");
+        //Setting the schedule
+        Practice mondayPractice = new Practice(6.00);
+        Practice tuesdayPractice = new Practice(6.00);
+        Practice wednesdayPractice = new Practice(5.15);
+        Practice thursdayPractice = new Practice(5.15);
+        planner.add(mondayPractice);
+        System.out.println(mondayPractice.time + " PM is when practice will be held.");
         System.out.println("Hello World!");
 
     }
